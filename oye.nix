09427be3245abe395e6ced3530874302e0c3b803
sdhou <xiaohou.don@gmail.com>
2024-03-14 17:19:34 +0800

    up nixos conf

--- oye.nix
+++ oye.nix
@@ -2,40 +2,65 @@
 with lib;
 {
   time.timeZone = "Asia/Shanghai";
+  i18n.defaultLocale = "zh_CN.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "zh_CN.UTF-8";
+    LC_IDENTIFICATION = "zh_CN.UTF-8";
+    LC_MEASUREMENT = "zh_CN.UTF-8";
+    LC_MONETARY = "zh_CN.UTF-8";
+    LC_NAME = "zh_CN.UTF-8";
+    LC_NUMERIC = "zh_CN.UTF-8";
+    LC_PAPER = "zh_CN.UTF-8";
+    LC_TELEPHONE = "zh_CN.UTF-8";
+    LC_TIME = "zh_CN.UTF-8";
+  };
+  networking.firewall.enable = false;
+  services.openssh.enable = true;
   nix.settings.substituters = [ "https://mirrors.cernet.edu.cn/nix-channels/store" ];
   documentation.enable = true;
   documentation.nixos.enable = true;
+  #security.sudo.extraRules= [
+  #  { users = [ "sdhou" ];
+  #    commands = [
+  #      { command = "ALL";
+  #        options= [ "NOPASSWD" ];
+  #      }
+  #    ];
+  #  }
+  #];
   environment.noXlibs = false;
   environment.systemPackages = with pkgs; [
-    vim
     axel
-    wget
-    git
-    cloc
-    tree
     bat
-    redis
-    nginx
+    cloc
     docker
-    nodePackages_latest.nodejs
-    pm2
-    yarn
-    nodePackages.pnpm
-    jdk
-    maven
     go
-    rclone
-    wrk
-    tig
-    #telnet
+    git
+    htop
+    jdk
+    jq
     mariadb
     minio
     mycli
-    jq
+    maven
+    nginx
+    nodePackages_latest.nodejs
+    nodePackages.pnpm
+    pm2
+    vim
+    wget
+    tree
+    unzip
+    redis
+    yarn
+    rclone
+    wrk
+    tig
   ];
   services.redis.servers."".enable = true;
   system.stateVersion = "23.11";
   virtualisation.docker.enable = true;
+  virtualisation.docker.enableOnBoot = true;
   users.users.sdhou = {
     isNormalUser = true;
     extraGroups = [
